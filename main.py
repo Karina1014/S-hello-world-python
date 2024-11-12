e9b9b11b6c0ef2ce328b1febcf0c224d661d88ca
Karina1014 <janneth_wi15@hotmail.com>
2024-11-12 13:16:08 -0500

    feat: :wrench: add .gitignore
    add .gitignore to exclude virtual environment and other unnecessary files 📝

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hellow World")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(port=5000)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(port=5000)
